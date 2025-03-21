0bbe9f9ddc56f3179b51007417518eb07af4e269
Nawa phansaen <79089602+nawaphan123@users.noreply.github.com>
2025-03-22 02:06:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "RapbitPro",
+  platform: "arduino-esp32",
+  title: "RapbitPro",
+  description: "หุ่นยนต์แห่งการเรียนรู้",
+  author: "Ratthanin Kittisriphong",
+  website: "https://PrinceBot.net",
+  email: "print081@gmail.com",
+  git: "https://github.com/PrinceBot-Ratthanin/RapbitPro/",
+  image: "/static/RapbitPro.png",
+  version: "1.0.0",
+};
