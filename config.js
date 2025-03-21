42fbb0bb95b12980b3f13544e178dba65a62ba29
Nawa phansaen <79089602+nawaphan123@users.noreply.github.com>
2025-03-22 02:12:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: "RapbitPro",
+  name: "RapbitProV_ETC",
   platform: "arduino-esp32",
-  title: "RapbitPro",
+  title: "RapbitProV_ETC",
   description: "หุ่นยนต์แห่งการเรียนรู้",
-  author: "Ratthanin Kittisriphong",
-  website: "https://PrinceBot.net",
-  email: "print081@gmail.com",
+  author: "Ratthanin Kittisriphong mod by Nawa Phansaen",
+  website: "",
+  email: "",
   git: "https://github.com/PrinceBot-Ratthanin/RapbitPro/",
   image: "/static/RapbitPro.png",
-  version: "1.0.0",
+  version: "1.0.1",
 };
